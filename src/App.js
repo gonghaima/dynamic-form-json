31207bf255c5399ff62cd44d29fbb9749bbeb268
Steven <gonghaima@hotmail.com>
2020-12-09 12:47:00 +1100

    include form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
+import { Form, TextField, SelectField, SubmitButton } from './FormElements';
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  const onSubmit = (values, { setSubmitting, resetForm, setStatus }) => {
+    console.log(values);
+    setSubmitting(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        enableReinitialize
+        initialValues={formData}
+        onSubmit={onSubmit}
+      >
+        <div>
+          <TextField
+            name="name"
+            label="Name"
+          />
+        </div>
+
+        <div>
+          <TextField
+            name="email"
+            label="Email"
+          />
+        </div>
+
+        <div>
+          <SelectField
+            name="role"
+            label="Role"
+            options={[
+              {
+                label: "Admin",
+                value: "admin"
+              },
+              {
+                label: "User",
+                value: "user"
+              }
+            ]}
+          />
+        </div>
+
+        <SubmitButton
+          title="Submit"
+        />
+      </Form>
     </div>
   );
 }
